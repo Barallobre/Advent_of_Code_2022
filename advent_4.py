7218aae9c031ad3e939766acfdc1f45bf9296b0a
Barallobre <cristianbarallobrerua@gmail.com>
2022-12-04 17:44:22 +0100

    day 4 refactor

--- advent_4.py
+++ advent_4.py
@@ -10,31 +10,36 @@ for character in pairs:
         pairs_of_work.append(element)
         element = ''
 
-print(pairs_of_work)
-
 elf_1_numbers = ''
 elf_2_numbers = ''
 elf_1_individual_numbers = []
 elf_2_individual_numbers = []
 
-not_matches = 0
-matches = len(pairs_of_work)
-for pair in pairs_of_work:
-    splited = pair.split(',')
-    print(splited)
-    elf_1_numbers = splited[0].split('-')
-    elf_2_numbers = splited[1].split('-')
-    print(elf_1_numbers)
-    print(elf_2_numbers)
 
-    for elf_1 in range(int(elf_1_numbers[0]), (int(elf_1_numbers[1]) + 1)):
-        elf_1_individual_numbers.append(elf_1)
-    print(elf_1_individual_numbers)
+def addNumbers_to_list(numbers):
+    individual_numbers = []
+    for elf_1 in range(int(numbers[0]), (int(numbers[1]) + 1)):
+        individual_numbers.append(elf_1)
+    return individual_numbers
 
-    for elf_2 in range(int(elf_2_numbers[0]), (int(elf_2_numbers[1]) + 1)):
-        elf_2_individual_numbers.append(elf_2)
-    print(elf_2_individual_numbers)
 
+def overlaps_per_pair(elf_1_individual_numbers, elf_2_individual_numbers):
+    overlaps = 0
+    if elf_1_individual_numbers[0] <= elf_2_individual_numbers[0]:
+        for number in elf_1_individual_numbers:
+            if number in elf_2_individual_numbers:
+                overlaps = overlaps + 1
+                break
+    else:
+        for number in elf_2_individual_numbers:
+            if number in elf_1_individual_numbers:
+                overlaps = overlaps + 1
+                break
+    return overlaps
+
+
+def not_matches_per_pair(elf_1_individual_numbers, elf_2_individual_numbers):
+    not_matches = 0
     if len(elf_1_individual_numbers) <= len(elf_2_individual_numbers):
         for number in elf_1_individual_numbers:
             if number not in elf_2_individual_numbers:
@@ -45,44 +50,59 @@ for pair in pairs_of_work:
             if number not in elf_1_individual_numbers:
                 not_matches = not_matches + 1
                 break
+    return not_matches
 
-    matches = matches - not_matches
-    not_matches = 0
-    print(matches)
-    elf_1_individual_numbers = []
-    elf_2_individual_numbers = []
 
-overlaps = 0
-overlaps_per_pair = 0
-for pair in pairs_of_work:
+def split_numbers(pair):
     splited = pair.split(',')
-    print(splited)
     elf_1_numbers = splited[0].split('-')
     elf_2_numbers = splited[1].split('-')
-    print(elf_1_numbers)
-    print(elf_2_numbers)
+    return elf_1_numbers, elf_2_numbers
 
-    for elf_1 in range(int(elf_1_numbers[0]), (int(elf_1_numbers[1]) + 1)):
-        elf_1_individual_numbers.append(elf_1)
-    print(elf_1_individual_numbers)
 
-    for elf_2 in range(int(elf_2_numbers[0]), (int(elf_2_numbers[1]) + 1)):
-        elf_2_individual_numbers.append(elf_2)
-    print(elf_2_individual_numbers)
+def matches_total():
+    not_matches_pair = 0
 
-    if elf_1_individual_numbers[0] <= elf_2_individual_numbers[0]:
-        for number in elf_1_individual_numbers:
-            if number in elf_2_individual_numbers:
-                overlaps = overlaps + 1
-                break
+    matches = len(pairs_of_work)
+    for pair in pairs_of_work:
 
-    else:
-        for number in elf_2_individual_numbers:
-            if number in elf_1_individual_numbers:
-                overlaps = overlaps + 1
-                break
+        elf_1_numbers = split_numbers(pair)[0]
+        elf_2_numbers = split_numbers(pair)[1]
 
-    elf_1_individual_numbers = []
-    elf_2_individual_numbers = []
+        elf_1_individual_numbers = addNumbers_to_list(elf_1_numbers)
+        elf_2_individual_numbers = addNumbers_to_list(elf_2_numbers)
 
+        not_matches_pair = not_matches_per_pair(
+            elf_1_individual_numbers, elf_2_individual_numbers)
+
+        matches = matches - not_matches_pair
+
+        elf_1_individual_numbers = []
+        elf_2_individual_numbers = []
+    return matches
+
+
+def overlaps_total():
+    overlaps_pair = 0
+    overlaps = 0
+    for pair in pairs_of_work:
+        elf_1_numbers = split_numbers(pair)[0]
+        elf_2_numbers = split_numbers(pair)[1]
+
+        elf_1_individual_numbers = addNumbers_to_list(elf_1_numbers)
+        elf_2_individual_numbers = addNumbers_to_list(elf_2_numbers)
+
+        overlaps_pair = overlaps_per_pair(
+            elf_1_individual_numbers, elf_2_individual_numbers)
+        elf_1_individual_numbers = []
+        elf_2_individual_numbers = []
+        overlaps = overlaps + overlaps_pair
+    return overlaps
+
+
+matches = 0
+matches = matches_total()
+print(matches)
+overlaps = 0
+overlaps = overlaps_total()
 print(overlaps)
